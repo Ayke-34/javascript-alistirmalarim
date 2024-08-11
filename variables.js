8f1bc37d306c169c9c81c9baf29ae5f0dc231a0c
Ayke-34 <aksoyaybuke12@gmail.com>
2024-08-11 09:26:32 +0300

    variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+const sayisalIfade = 66;
+const sozelIfade = 'aybuke';
+//iki sayının toplanıp ortalamasının bulunması
+var ilksayi= 5;
+var ikincisayi =20;
+var toplam = ilksayi + ikincisayi ;
+var ortalama= toplam /2;
+//ad ve soyad değerlerinin tutulduğu iki değişken oluşturulup sonra birleştirilip ekrana yazdırılması
+var ad = "aybuke"
+var soyad = "aksoy"
+var son = ad + "  " + soyad;
+alert(son);
+//kişisel bilgilerin bulunduğu bir obje tasarlanması
+const person = {firstname:"aybuke", lastname: "aksoy", yas: 21};
+//market listesi için bir array tasarlanması
+const market = ["elma", "armut", "muz","karpuz"];
+alert (market[2])
+alert (person.firstname + "  " + person.yas )
+alert(ikincisayi+person.yas)
