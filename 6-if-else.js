c3e6af95ff534b84dad2d1b1e01909d1036a996e
Ayke-34 <aksoyaybuke12@gmail.com>
2024-08-13 21:25:44 +0300

    genel yapısı hakkında örnek

---
+++ 6-if-else.js
@@ -0,0 +1,24 @@
+const username = "aybukeaksoy"
+const password = "123456"
+if (username == "aybukeaksoy") {
+    if (password == "123456") {
+        console.log("Giriş Yapıldı.");
+    } else{
+        console.log("Şifre Yanlış!");
+    }
+}else{
+    console.log("Kullanıcı Bulunamadı.");
+    
+}
+
+
+
+
+//let isLoggedIn = true;
+// if(isLoggedIn == true) {
+//     console.log("calisti");
+// }else{
+//     console.log("true degil");
+// }
+// console.log("Cikti");
+
---
+++ 7-if-else-2.js
@@ -0,0 +1,14 @@
+let yas = 17;
+let mezuniyet = "üniversite";
+if(yas>=18 && (mezuniyet == "lise" || mezuniyet == "üniversite" )){
+console.log("Ehliyet şarlarını karşıladığın için ehliyet alabilirsin.");
+
+}else if (yas===17){
+    console.log("Dayan. Son bir senen.");
+    
+}
+
+else{
+    console.log("Ehliyet şarlarını karşılamadığın için ehliyet alamazsın!");
+    
+}
