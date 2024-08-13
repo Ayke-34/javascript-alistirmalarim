dfccfa61985a06175746da2877ebd12b52ff9127
Ayke-34 <aksoyaybuke12@gmail.com>
2024-08-13 17:08:56 +0300

    genel yapısı hakkında örnek

---
+++ 2-degiskenler.js
@@ -0,0 +1,14 @@
+//let Muduradi = "Ahmet";
+//let Sinifbaskani = "Emin";
+
+//console.log("Müdür Adı:" + Muduradi);
+//console.log("Sınıf Başkanı:" + Sinifbaskani);
+
+let maas = 5500;
+let zam = 0.2;
+let ZamlıMaas = maas + maas*zam;
+console.log(ZamlıMaas);
+
+
+
+
---
+++ 3-degisken-turleri.js
@@ -0,0 +1,10 @@
+//let OgrenciIsmi = " Ahmet Yılmaz"
+//let OgrenciYasi = 20
+//console.log(typeof OgrenciIsmi);
+//console.log(typeof OgrenciYasi);
+let sayi1 = "10";
+let sayi2 = "20";
+let toplamu = Number(sayi1) + Number(sayi2) ;
+console.log(toplamu);
+
+
