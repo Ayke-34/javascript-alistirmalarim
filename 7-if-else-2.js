ac1cf7755b0dc6f121e8ca1472a5c920606c4e70
Ayke-34 <aksoyaybuke12@gmail.com>
2024-08-14 22:20:55 +0300

    strings ve backtick örneği

--- 7-if-else-2.js
+++ 7-if-else-2.js
@@ -1,34 +1,31 @@
-let yas = 17;
-let ad = "  mehmet"
-let mezuniyet = "üniversite";
+// let yas = 17;
+// let ad = "  mehmet"
+// let mezuniyet = "üniversite";
 
-
-
-
-
-
-
-
-{let soyad = "yılmaz"
-    console.log(soyad);
-     var tc=12345678; 
+// {let soyad = "yılmaz"
+//     console.log(soyad);
+//      var tc=12345678; 
     
-}
-if(yas>=18 && (mezuniyet == "lise" || mezuniyet == "üniversite" )){
-console.log("Ehliyet şarlarını karşıladığın için ehliyet alabilirsin.");
+// }
+// if(yas>=18 && (mezuniyet == "lise" || mezuniyet == "üniversite" )){
+// console.log("Ehliyet şarlarını karşıladığın için ehliyet alabilirsin.");
 
-}else if (yas===17){
-    console.log(tc);
-    console.log("Dayan. Son bir senen.");
-    console.log(ad);
-    console.log(soyad);
+// }else if (yas===17){
+//     console.log(tc);
+//     console.log("Dayan. Son bir senen.");
+//     console.log(ad);
+//     console.log(soyad);
    
     
     
     
-}
+// }
 
-else{
-    console.log("Ehliyet şarlarını karşılamadığın için ehliyet alamazsın!");
+// else{
+//     console.log("Ehliyet şarlarını karşılamadığın için ehliyet alamazsın!");
     
-}
+// }
+
+// function deneme (){
+    
+// }
---
+++ 8-strings.js
@@ -0,0 +1,11 @@
+const ad = "Aybuke";
+const soyad = "Aksoy";
+const yas = 21;
+const sehir = "İstanbul";
+const meslek = "Full-Stack Developer";
+const bio = "Benim adım " + ad + " soyadım " + soyad +". " + sehir + "'da yaşıyorum " + yas + " yaşındayım ";
+console.log(bio);
+
+//! Backtick
+const newBio = `Benim adım ${ad} soyadım ${soyad}. ${sehir}'da yaşıyorum ${yas} yaşındayım ${meslek} olarak çalışıyorum `
+console.log(newBio);
