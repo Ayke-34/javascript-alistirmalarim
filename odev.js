4404f42a76f11caa7e4ce8a3ef39160e2d8b15c8
Ayke-34 <aksoyaybuke12@gmail.com>
2024-08-17 09:09:16 +0300

    sorular

--- odev.js
+++ odev.js
@@ -7,22 +7,54 @@
 // 3.soru-boş olarak oluşturulan bir arraye 1den 50ye kadar olan sadece tek sayılar döngüyle eklenecektir
 //let a=[]
 
+// 4.soru- [0, 3, 7, 2, 11, 16, 18, 20, 9, 36, 45] 
+//bu küme içerisindeki tek sayıları bulan ve yeni bir kümeye ekleyen kod yazın.
+
 //1. soru
-//let yol = 1000
+let yol = 1000;
+let ortalamaYakıt = 10; //100km için tutulmuş bir ortalama
+let harcananYakıt ; 
+harcananYakıt = (yol/100) *ortalamaYakıt;
+console.log(harcananYakıt);
 
 
 //2.soru
-// const number = [1, 2, 3, 4, 5, 6, 7, 8];
-// for (let i=1; i<8 i=i+2){
-// console.log(number[i])
+const number = [1, 2, 3, 4, 5, 6, 7, 8];
+for (let i=1; i<8 ; i=i+2){
+console.log(number[i])
      
-//  }
+ }
 
 //3.soru
-// let kume = [];
-// for (let i= 1; i<= 50; i += 2){
-//     alert(kume[i]);
-// }
+let kume = [];
+for (let i= 1; i<= 50; i += 2){
+    kume.push(i);
+ }
+console.log(kume);
+
+//4. soru
+let eskisayilar =[0, 3, 7, 2, 11, 16, 18, 20, 9, 36, 45];
+let teksayilar= []
+let ciftsayilar= []
+for (let i=0 ; i<eskisayilar.length ; i++){
+if(eskisayilar[i] % 2 !=0){
+teksayilar.push(eskisayilar[i])
+}
+else{
+   
+    ciftsayilar.push(eskisayilar[i])
+}
+
+
+
+}
+console.log(teksayilar);
+
+console.log(ciftsayilar);
+
+
+    
+
 
 
 
