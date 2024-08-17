770a637ef73f649aa80d5a4a633dba9ecdde2f6b
Ayke-34 <aksoyaybuke12@gmail.com>
2024-08-17 09:35:42 +0300

    ternary örnek

--- 11-ternary-operatoru.js
+++ 11-ternary-operatoru.js
@@ -1,7 +1,7 @@
 // const username =  "aybukeaksoy";
-// const password = "1234567";
+const password = "123456789";
 
-// password.length >6 ? console.log("çalıştı") : console.log("çalışmadı");
+//password.length >6 ? console.log("çalıştı") : console.log("çalışmadı");
 // username.length >13 ? console.log("username doğru") : console.log("username yanlış");
 
 
@@ -12,3 +12,8 @@
 //     console.log("6'dan büyük olmadığı için çalışmadı");
 // }
 
+ //sadece if olucaksa
+//password.length<8 && console.log("şifre 8den küçük şifrenizi güncelleyin");
+ 
+//şifre 8den büyükse şifre uyumlu devam edin
+password.length>8 && console.log("şifre 8den büyük devam edin");
