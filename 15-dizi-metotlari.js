950cc5d7def723e5f7088950340c33af88a9021c
Ayke-34 <aksoyaybuke12@gmail.com>
2024-08-18 19:14:44 +0300

    dizi metotları

---
+++ 15-dizi-metotlari.js
@@ -0,0 +1,34 @@
+// let products = ["iphone5", "samsung", "huawei"];
+// let result ;
+
+// result = products.length;
+
+//array to string 
+// result = products.toString(); //stringe çevir
+// result = products.join("/"); //ayırma
+
+//diziden eleman silme
+//result = products.shift(); //ilk elemanı siler geri döndürür
+//result = products.pop(); //son elemanı siler geri döndürür
+
+
+//diziye eleman ekleme
+//result = products.push("xiaomi"); //dizi sonuna eleman ekler
+//result = products.unshift("xiaomi"); //dizi başına eleman ekler
+
+//dizi birleştirme
+
+// let urunler1 = ["asus", "apple"];
+// let urunler2 = ["dell", "casper"];
+// let urunler3 = ["acer", "exper"];
+
+//result = urunler1.concat(urunler2, urunler3);
+//result = urunler1.splice(0, 0, urunler2);
+//result = urunler1.splice(0, 0, "elma"); //?????????????????????????????
+// result = urunler1.splice(0, 1);
+
+
+
+// console.log(result);
+// console.log(urunler1);
+
