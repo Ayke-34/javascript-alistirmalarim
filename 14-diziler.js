3b9e278641a44eec0e6ede0663632b3a2f80295b
Ayke-34 <aksoyaybuke12@gmail.com>
2024-08-18 18:16:29 +0300

    diziler

---
+++ 14-diziler.js
@@ -0,0 +1,14 @@
+// const urun1 = "apple";
+// const urun2 = "samsung";
+// const urun3 = "huawei";
+
+const urunler = ["samsung", "apple", "huawei"];
+const fiyatlar = [10, 20, 30];
+const YeniUrun = ["samsung", 150, ["apple", 20]];
+ 
+
+
+//console.log(YeniUrun[2][1]); //20 yazar
+console.log(urunler[2] + " " + "=" + " " + fiyatlar[1]);
+
+ 
