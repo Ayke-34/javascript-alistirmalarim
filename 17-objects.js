23fd7fc5c2f5cb98eb813221c4b2a6109754ae82
Ayke-34 <aksoyaybuke12@gmail.com>
2024-09-01 18:17:13 +0300

    objeler

---
+++ 17-objects.js
@@ -0,0 +1,52 @@
+// let customer = {
+// "ad": "Emin",
+// "soyad": "BaŞbayan",
+// "sehir": "İstanbul",
+// "yas": 25
+// };
+
+// let customer2 = {
+//     "ad": "Nur",
+//     "soyad": "BaŞbayan",
+//     "sehir": "İstanbul",
+//     "yas": 25,
+//     "products":{
+//         "phone": "ıphone",
+//         "price": 10
+
+//     }
+//     };
+
+//     let customer1 = {
+//         "ad": "Bedirhan",
+//         "soyad": "BaŞbayan",
+//         "sehir": "İstanbul",
+//         "yas": 25
+//         };
+
+
+
+//         let urunler = [
+//             {
+// "id" : 1,
+// "urunadi": "huawei",
+// "urunfiyati": 10,
+//             },
+            
+//             {
+// "id" : 2,
+// "urunadi": "apple",
+// "urunfiyati": 20,
+//             },
+            
+            
+//             {
+// "id": 3,
+// "urunadi": "vestel",
+// "urunfiyati": 30
+
+//             },
+
+//         ]
+// console.log(urunler);
+        
